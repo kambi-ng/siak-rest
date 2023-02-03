236fcbd2c48fa1441fa141eae41492f7ebb81a6f
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-02-03 22:33:44 +0700

    feat:a added login endpoint

--- go.mod
+++ go.mod
@@ -2,14 +2,16 @@ module github.com/kambi-ng/siak-rest
 
 go 1.19
 
-require github.com/PuerkitoBio/goquery v1.8.0
+require (
+	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/gofiber/fiber/v2 v2.42.0
+)
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
-	github.com/gofiber/fiber/v2 v2.42.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/klauspost/compress v1.15.15 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
--- http-server/handler.go
+++ http-server/handler.go
@@ -1,7 +1,58 @@
 package httpserver
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Login(c *fiber.Ctx) error {
-  return c.SendString("Hello, World!")
+	type Payload struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	var p Payload
+
+	if err := c.BodyParser(&p); err != nil {
+		return err
+	}
+
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		return err
+	}
+	client := &http.Client{
+		Jar: jar,
+	}
+
+	if _, err = client.PostForm("https://academic.ui.ac.id/main/Authentication/Index", url.Values{"u": {p.Username}, "p": {p.Password}}); err != nil {
+		return err
+	}
+
+	resp, err := client.Get("https://academic.ui.ac.id/main/Authentication/ChangeRole")
+	if err != nil {
+		return err
+	}
+
+	for h := range resp.Header {
+		c.Set(h, resp.Header.Get(h))
+	}
+	for _, cookie := range jar.Cookies(resp.Request.URL) {
+		cookies := &fiber.Cookie{}
+		cookies.Name = cookie.Name
+		cookies.Value = cookie.Value
+		cookies.Path = cookie.Path
+		cookies.Domain = cookie.Domain
+		cookies.MaxAge = cookie.MaxAge
+		cookies.Expires = cookie.Expires
+		cookies.Secure = cookie.Secure
+		cookies.HTTPOnly = cookie.HttpOnly
+
+		c.Cookie(cookies)
+	}
+
+	return c.Status(resp.StatusCode).Send(nil)
 }
