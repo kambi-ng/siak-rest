9760a5d5b1ff96168e6db49b9595e5fb4e76977c
Rendy Arya Kemal <renrror@gmail.com>
2023-02-28 20:25:54 +0700

    Add rate limiter using redis

--- go.mod
+++ go.mod
@@ -8,8 +8,15 @@ require (
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/redis/go-redis/v9 v9.0.2 // indirect
+)
+
+require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/gofiber/storage/redis v1.3.4
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.15.15 // indirect
--- http-server/server.go
+++ http-server/server.go
@@ -2,9 +2,12 @@ package httpserver
 
 import (
 	"os"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/storage/redis"
 )
 
 type Server struct {
@@ -19,6 +22,38 @@ func MakeServer() Server {
 
 func (s *Server) Start() {
 	allowedOrigins := os.Getenv("ALLOW_ORIGINS")
+	redisUrl := os.Getenv("REDIS_URL")
+
+	var limiterConfig limiter.Config
+	if redisUrl == "" {
+		redisStore := redis.New(redis.Config{
+			URL:   redisUrl,
+			Reset: false,
+		})
+
+		limiterConfig = limiter.Config{
+			Next: func(c *fiber.Ctx) bool {
+				return c.IP() == "127.0.0.1"
+			},
+			Max:        30,
+			Expiration: 60 * time.Second,
+			KeyGenerator: func(c *fiber.Ctx) string {
+				return c.IP()
+			},
+			LimitReached: func(c *fiber.Ctx) error {
+				return c.Status(fiber.StatusTooManyRequests).JSON(Response[any]{
+					Status:  -1,
+					Message: "Rate limited",
+					Data:    nil,
+				})
+			},
+			Storage: redisStore,
+		}
+	} else {
+		limiterConfig = limiter.ConfigDefault
+	}
+
+	s.Router.Use(limiter.New(limiterConfig))
 
 	if allowedOrigins == "" {
 		s.Router.Use(cors.New())
